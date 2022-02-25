bddef2eb82b9105fc382ad02307812ee2fa46b96
Naima Hassan <naima.hassan434@gmail.com>
2022-02-25 19:39:58 +0000

    User Schema create&model

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ mongoose
   .connect("mongodb://localhost:27017/allInOne")
   .then(() => console.log("Database is connected"))
   .catch((err) => console.error(err));
+
+//User Schema Create
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+});
+//user model
+const User = mongoose.model("User", userSchema);
