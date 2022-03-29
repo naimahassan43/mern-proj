2d64e5955f104450995751727813b57e10562859
Naima Hassan <naima.hassan434@gmail.com>
2022-03-29 21:59:31 +0100

    REST API Design with Express

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const express = require("express");
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+const app = express();
 mongoose
   .connect("mongodb://localhost:27017/allInOne")
   .then(() => console.log("Database is connected"))
@@ -9,17 +11,26 @@ mongoose
 const User = require("./model/User");
 const Task = require("./model/Task");
 
-async function db() {
-  try {
-    const task = new Task({
-      description: "Task",
-      isCompleted: true,
-    });
+const port = process.env.PORT || 4040;
+app.listen(port, () => console.log(`Server is running at port ${port}`));
+/*
+  /task POST
+  /task GET
+  /task/:id GET
+  /task/:id PATCH
+  /task/:id DELETE
+*/
+// async function db() {
+//   try {
+//     const task = new Task({
+//       description: "Task",
+//       isCompleted: true,
+//     });
 
-    await task.save();
-    console.log(task);
-  } catch (err) {
-    console.log(colors.red.underline.bold(err.message));
-  }
-}
-db();
+//     await task.save();
+//     console.log(task);
+//   } catch (err) {
+//     console.log(colors.red.underline.bold(err.message));
+//   }
+// }
+// db();
