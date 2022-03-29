c73fd24fadf72c700fe7ec5cabfe1b1e7f9de1eb
Naima Hassan <naima.hassan434@gmail.com>
2022-03-29 22:34:11 +0100

    Store User and Task Info

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+
 mongoose
   .connect("mongodb://localhost:27017/allInOne")
   .then(() => console.log("Database is connected"))
@@ -10,6 +14,28 @@ mongoose
 
 const User = require("./model/User");
 const Task = require("./model/Task");
+
+app.post("/task", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+
+    return res.status(201).json({ success: true, task });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    return res.status(201).json({ success: true, user });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
+});
 
 const port = process.env.PORT || 4040;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
@@ -20,17 +46,3 @@ app.listen(port, () => console.log(`Server is running at port ${port}`));
   /task/:id PATCH
   /task/:id DELETE
 */
-// async function db() {
-//   try {
-//     const task = new Task({
-//       description: "Task",
-//       isCompleted: true,
-//     });
-
-//     await task.save();
-//     console.log(task);
-//   } catch (err) {
-//     console.log(colors.red.underline.bold(err.message));
-//   }
-// }
-// db();
